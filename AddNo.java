2bb8b76936b7ef8403110907576507f3fcfa412d
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-07-31 08:33:45 +0530

    Initial commit

---
+++ AddNo.java
@@ -0,0 +1,8 @@
+public class AddNo{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("he addition of two number is:"+c);
+    }
+}
